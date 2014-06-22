874dbdecf9e68cbcd8b28f340eec025ddc19c736
Artem Abashev <artem@abashev.com>
2014-06-22 18:50:39 +0400

    Added Touch.Notification.Aws.

---
+++ Touch.Notification.Aws/BroadcasterProvider.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Concurrent;
+using Amazon;
+using Amazon.Runtime;
+using Amazon.SimpleNotificationService;
+using Touch.Serialization;
+
+namespace Touch.Notification
+{
+    /// <summary>
+    /// Broadcaster provider.
+    /// </summary>
+    sealed public class BroadcasterProvider : IBroadcasterProvider
+    {
+        #region .ctor
+        public BroadcasterProvider(ISerializer serializer, AWSCredentials credentials)
+        {
+            if (serializer == null) throw new ArgumentNullException("serializer");
+            _serializer = serializer;
+
+            if (credentials == null) throw new ArgumentException("credentials");
+            _credentials = credentials;
+        }
+        #endregion
+
+        #region Data
+        private readonly ISerializer _serializer;
+        private readonly AWSCredentials _credentials;
+        private readonly ConcurrentDictionary<string, object> _broadcasters = new ConcurrentDictionary<string, object>();
+        #endregion;
+
+        #region IBroadcasterProvider implementation
+        private IAmazonSimpleNotificationService ClientFactory()
+        {
+            return AWSClientFactory.CreateAmazonSimpleNotificationServiceClient(_credentials);
+        }
+
+        public INotificationBroadcaster<T> GetBroadcaster<T>(string topicName) 
+            where T : class, INotification, new()
+        {
+            if (string.IsNullOrEmpty(topicName)) throw new ArgumentException("topicName");
+
+            object broadcaster;
+
+            if (!_broadcasters.TryGetValue(topicName, out broadcaster))
+            {
+                broadcaster = new NotificationBroadcaster<T>(_serializer, ClientFactory, topicName);
+                _broadcasters[topicName] = broadcaster;
+            }
+
+            return (INotificationBroadcaster<T>)broadcaster;
+        }
+        #endregion
+    }
+}
---
+++ Touch.Notification.Aws/NotificationBroadcaster.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using Amazon.SimpleNotificationService;
+using Amazon.SimpleNotificationService.Model;
+using Amazon.SimpleNotificationService;
+using Touch.Serialization;
+
+namespace Touch.Notification
+{
+    /// <summary>
+    /// Amazon SNS notification broadcaster.
+    /// </summary>
+    /// <typeparam name="T">Notification type.</typeparam>
+    internal sealed class NotificationBroadcaster<T> : INotificationBroadcaster<T>
+        where T : class, INotification, new()
+    {
+        #region .ctor
+        /// <summary>
+        /// Amazon SNS notification broadcaster contructor.
+        /// </summary>
+        /// <param name="serializer">Notification message serializer.</param>
+        /// <param name="clientFactory">Notification client factory.</param>
+        /// <param name="notificationTopic">Notification topic name.</param>
+        internal NotificationBroadcaster(ISerializer serializer, Func<IAmazonSimpleNotificationService> clientFactory, string notificationTopic)
+        {
+            if (serializer == null) throw new ArgumentNullException("serializer");
+            _serializer = serializer;
+
+            if (clientFactory == null) throw new ArgumentNullException("clientFactory");
+            _clientFactory = clientFactory;
+
+            if (string.IsNullOrEmpty(notificationTopic)) throw new ArgumentException("notificationTopic");
+            _notificationTopic = notificationTopic;
+        }
+        #endregion
+
+        #region Data
+        private readonly ISerializer _serializer;
+        private readonly Func<IAmazonSimpleNotificationService> _clientFactory;
+        private readonly string _notificationTopic;
+        #endregion;
+
+        #region INotificationBroadcaster implementation
+        public void Broadcast(T notification)
+        {
+            if (notification == null) throw new ArgumentNullException("notification");
+
+            var messageBody = _serializer.Serialize(notification);
+
+            using (var client = _clientFactory.Invoke())
+            {
+                var request = new PublishRequest
+                {
+                    TopicArn = _notificationTopic,
+                    Message = messageBody
+                };
+
+                client.Publish(request);
+            }
+        }
+        #endregion;
+    }
+}
