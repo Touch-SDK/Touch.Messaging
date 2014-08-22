228c6a08a7f361d2147e0eeae8739a01f9ed459d
Artem Abashev <artem@abashev.com>
2014-08-22 17:38:34 +0400

    AWS SES e-mail sender.

---
+++ Touch.Email.Ses/AmazonEmailSender.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Linq;
+using Amazon;
+using Amazon.Runtime;
+using Amazon.SimpleEmail.Model;
+
+namespace Touch.Email
+{
+    public sealed class AmazonEmailSender : IEmailSender
+    {
+        #region .ctor
+        public AmazonEmailSender(AWSCredentials credentials, string connectionString)
+        {
+            if (credentials == null) throw new ArgumentNullException("credentials");
+            if (string.IsNullOrWhiteSpace(connectionString)) throw new ArgumentNullException("connectionString");
+
+            _config = new SesConnectionStringBuilder { ConnectionString = connectionString };
+            if (string.IsNullOrWhiteSpace(_config.SenderAddress)) throw new ArgumentException("SenderAddress is not set.", "connectionString");
+
+            _credentials = credentials;
+        }
+        #endregion
+
+        #region Data
+        private readonly SesConnectionStringBuilder _config;
+        private readonly AWSCredentials _credentials;
+        #endregion
+
+        #region IEmailSender members
+        public void Send(EmailMessage message)
+        {
+            if (message == null) throw new ArgumentNullException("message");
+
+            var destination = new Destination { ToAddresses = message.Recipients.ToList() };
+
+            var subject = new Content(message.Subject);
+            var body = new Body(new Content(message.Body));
+
+            var email = new Message(subject, body);
+
+            var request = new SendEmailRequest
+            {
+                Destination = destination,
+                Message = email,
+                Source = _config.SenderAddress
+            };
+
+            using (var client = AWSClientFactory.CreateAmazonSimpleEmailServiceClient(_credentials, _config.Region))
+            {
+                client.SendEmail(request);
+            }
+        }
+        #endregion
+    }
+}
---
+++ Touch.Email.Ses/SesConnectionStringBuilder.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Amazon;
+
+namespace Touch.Email
+{
+    public class SesConnectionStringBuilder : DbConnectionStringBuilder
+    {
+        public string SenderAddress
+        {
+            get { return ContainsKey("SenderAddress") ? this["SenderAddress"] as string : null; }
+            set { this["SenderAddress"] = value; }
+        }
+
+        public RegionEndpoint Region
+        {
+            get { return RegionEndpoint.GetBySystemName(this["Region"] as string); }
+            set { this["Region"] = value; }
+        }
+    }
+}
